1f4dba90b2c18aeb2ac195d98cd0d69bd1c0d4da
Pat Maddox <patmaddox@me.com>
2013-08-20 12:31:36 -0500

    whitelist and blacklist stuff

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,9 +1,28 @@
 class Contact < ActiveRecord::Base
   validates :name, :presence => true
+  validate :whitelist_xor_blacklist
   scope :whitelisted, -> { where(:whitelist => true) }
   scope :blacklisted, -> { where(:blacklist => true) }
 
   def self.whitelisted?(phone_number)
     whitelisted.where(phone_number: phone_number).exists?
+  end
+
+  def whitelist
+    self.blacklist = false
+    self.whitelist = true
+  end
+
+  def blacklist
+    self.whitelist = false
+    self.blacklist = true
+  end
+
+  private
+  def whitelist_xor_blacklist
+    if whitelist? && blacklist?
+      errors.add(:whitelist, 'cannot be true along with blacklist')
+      errors.add(:blacklist, 'cannot be true along with whitelist')
+    end
   end
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -16,4 +16,43 @@ describe Contact do
       expect(Contact.whitelisted?("123")).to be_false
     end
   end
+
+  describe "validations" do
+    it "rejects a contact that is whitelisted and blacklisted" do
+      contact = Contact.new whitelist: true, blacklist: true, name: 'Pat'
+      contact.should_not be_valid
+      expect(contact).to have(1).error_on(:whitelist)
+      expect(contact).to have(1).error_on(:blacklist)
+    end
+  end
+
+  describe "whitelist" do
+    let(:contact) { Contact.new }
+
+    it "sets whitelist? to true" do
+      contact.whitelist
+      contact.should be_whitelist
+    end
+
+    it "sets blacklist? to false" do
+      contact.blacklist = true
+      contact.whitelist
+      contact.should_not be_blacklist
+    end
+  end
+
+  describe "blacklist" do
+    let(:contact) { Contact.new }
+
+    it "sets blacklist? to true" do
+      contact.blacklist
+      contact.should be_blacklist
+    end
+
+    it "sets whitelist? to false" do
+      contact.whitelist = true
+      contact.blacklist
+      contact.should_not be_whitelist
+    end
+  end
 end
