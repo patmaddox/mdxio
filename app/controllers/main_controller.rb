397e990bfb330de0639f44ceeb77d98f4d7d955b
Pat Maddox <patmaddox@me.com>
2013-08-20 10:42:16 -0500

    syntax stuff

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,9 @@
 class MainController < ApplicationController
   def home
-    render :text => 'I am Pat Maddox'
+    render text: 'I am Pat Maddox'
   end
 
   def twilio_callback
-    render :text => Twilio::Verb.dial ENV['MY_PHONE_NUMBER']
+    render text: Twilio::Verb.dial(ENV['MY_PHONE_NUMBER'])
   end
 end
